2f6572c73e2f8db7ca3e6fdcfe6e984be5095727
guru-venkat <guruvenkat96@gmail.com>
2022-07-16 19:48:18 +0530

    Day 4 part 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import enum
+import pprint
 
 
 class Day1:
@@ -378,6 +379,136 @@ class Day3:
         return self.submarine.lifeSupportRating
 
 
+class BingoBoard:
+    def __init__(self, board: list[str]):
+        self.board = [int(c.strip()) for r in board for c in r.split()]
+        self.markTracker = [[False] * 5 for _ in range(5)]
+        self.lastNumber = -1
+
+    def printBoard(self):
+        pprint.pprint([self.board[0:5]] + [self.board[5:10]] + [self.board[10:15]] +
+                      [self.board[15:20]] + [self.board[20:25]])
+        pprint.pprint(self.markTracker)
+
+    def checkBingo(self):
+        for row in self.markTracker:
+            if all(row):
+                return True
+
+        for col in map(tuple, zip(*self.markTracker)):
+            if all(col):
+                return True
+
+        return False
+
+    def processDraw(self, number_drawn):
+        if number_drawn in self.board:
+            index = self.board.index(number_drawn)
+            self.markTracker[(index // 5)][(index % 5)] = True
+            if self.checkBingo():
+                self.lastNumber = number_drawn
+                return True
+        return False
+
+    def calculateScore(self):
+        score = self._sumOfUnmarkedElements() * self.lastNumber
+        self.printBoard()
+        return score
+
+    def _sumOfUnmarkedElements(self) -> int:
+        unMarkedSum = 0
+        for i, a in enumerate(self.board):
+            if not self.markTracker[(i // 5)][(i % 5)]:
+                unMarkedSum += a
+
+        return unMarkedSum
+
+
+class Day4:
+    """
+    --- Day 4: Giant Squid --- You're already almost 1.5km (almost a mile) below the surface of the ocean,
+    already so deep that you can't see any sunlight. What you can see, however, is a giant squid that has attached
+    itself to the outside of your submarine.
+
+    Maybe it wants to play bingo?
+
+    Bingo is played on a set of boards each consisting of a 5x5 grid of numbers. Numbers are chosen at random,
+    and the chosen number is marked on all boards on which it appears. (Numbers may not appear on all boards.) If all
+    numbers in any row or any column of a board are marked, that board wins. (Diagonals don't count.)
+
+    The submarine has a bingo subsystem to help passengers (currently, you and the giant squid) pass the time. It
+    automatically generates a random order in which to draw numbers and a random set of boards (your puzzle input).
+    For example:
+
+    7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+    22 13 17 11  0
+     8  2 23  4 24
+    21  9 14 16  7
+     6 10  3 18  5
+     1 12 20 15 19
+
+     3 15  0  2 22
+     9 18 13 17  5
+    19  8  7 25 23
+    20 11 10 24  4
+    14 21 16 12  6
+
+    14 21 17 24  4
+    10 16 15  9 19
+    18  8 23 26 20
+    22 11 13  6  5
+     2  0 12  3  7
+
+    After the first five numbers are drawn (7, 4, 9, 5, and 11), there are no winners, but the boards are marked as
+    follows (shown here adjacent to each other to save space):
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+     8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+     6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+     1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+    After the next six numbers are drawn (17, 23, 2, 0, 14, and 21), there are still no winners:
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+     8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+     6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+     1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+    Finally, 24 is drawn:
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+     8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+     6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+     1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+    At this point, the third board wins because it has at least one complete row or column of marked numbers (in this
+    case, the entire top row is marked: 14 21 17 24 4).
+
+    The score of the winning board can now be calculated. Start by finding the sum of all unmarked numbers on that
+    board; in this case, the sum is 188. Then, multiply that sum by the number that was just called when the board
+    won, 24, to get the final score, 188 * 24 = 4512.
+
+    To guarantee victory against the giant squid, figure out which board will win first. What will your final score
+    be if you choose that board?
+    """
+    def __init__(self):
+        with open("Day4Data.txt", "r") as f:
+            self.data = tuple(f.read().split('\n\n'))
+            self.rolls = tuple(int(r) for r in self.data[0].split(','))
+            self.boards = [BingoBoard(board.split('\n')) for board in self.data[1:]]
+
+    def part1(self):
+        for roll in self.rolls:
+            for board in self.boards:
+                if board.processDraw(roll):
+                    print(board.calculateScore(), roll)
+                    exit(0)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(Day3().part2())
+    print(Day4().part1())
