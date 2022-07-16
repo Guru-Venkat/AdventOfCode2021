bc8380ec73b9a81398722d748dfd074ab088fb18
guru-venkat <guruvenkat96@gmail.com>
2022-07-16 15:53:27 +0530

    Day 3 part 1

--- main.py
+++ main.py
@@ -122,10 +122,16 @@ class Submarine:
     def location(self):
         return self.position * self.depth
 
+    @property
+    def powerConsumption(self):
+        return self.gammaRate * self.epsilonRate
+
     def __init__(self):
         self.position = 0
         self.depth = 0
         self.aim = 0
+        self.gammaRate = 0
+        self.epsilonRate = 0
 
     def processCommand(self, command, step):
         # noinspection PyUnusedLocal
@@ -139,6 +145,21 @@ class Submarine:
                 self.aim += step
             case default:
                 print(f"Invalid Command {command}")
+
+    def processDiagnostic(self, diagnostic_data):
+        data = tuple(map(tuple, zip(*diagnostic_data)))  # Transpose the 2D list
+        gammaRate = ''
+        epsilonRate = ''
+        for d in data:
+            if d.count('1') > len(diagnostic_data) / 2:
+                gammaRate += '1'
+                epsilonRate += '0'
+            else:
+                gammaRate += '0'
+                epsilonRate += '1'
+
+        self.gammaRate = int(gammaRate, 2)
+        self.epsilonRate = int(epsilonRate, 2)
 
 
 class Day2:
@@ -213,6 +234,60 @@ class Day2:
         return self.submarine.location
 
 
+class Day3:
+    """
+    --- Day 3: Binary Diagnostic ---
+    The submarine has been making some odd creaking noises, so you ask it to produce a diagnostic report just in case.
+
+    The diagnostic report (your puzzle input) consists of a list of binary numbers which, when decoded properly,
+    can tell you many useful things about the conditions of the submarine. The first parameter to check is the power
+    consumption.
+
+    You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma
+    rate and the epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon
+    rate.
+
+    Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all
+    numbers in the diagnostic report. For example, given the following diagnostic report:
+
+    00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010
+
+    Considering only the first bit of each number, there are five 0 bits and seven 1 bits. Since the most common bit
+    is 1, the first bit of the gamma rate is 1.
+
+    The most common second bit of the numbers in the diagnostic report is 0, so the second bit of the gamma rate is 0.
+
+    The most common value of the third, fourth, and fifth bits are 1, 1, and 0, respectively, and so the final three
+    bits of the gamma rate are 110.
+
+    So, the gamma rate is the binary number 10110, or 22 in decimal.
+
+    The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from
+    each position is used. So, the epsilon rate is 01001, or 9 in decimal. Multiplying the gamma rate (22) by the
+    epsilon rate (9) produces the power consumption, 198.
+    """
+
+    def __init__(self):
+        with open("Day3Data.txt", "r") as f:
+            self.data = tuple(f.read().splitlines())
+            self.submarine = Submarine()
+
+    def part1(self):
+        self.submarine.processDiagnostic(self.data)
+        return self.submarine.powerConsumption
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(Day2().part1())
+    print(Day3().part1())
