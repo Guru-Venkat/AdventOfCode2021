28d68ef024e1b788926ade8a2709268d37f60104
guru-venkat <guruvenkat96@gmail.com>
2022-07-16 23:45:33 +0530

    Day 7 (Copied approach)

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import math
+
+import numpy as np
+
 
 class Day6:
     """
@@ -66,7 +70,15 @@ class Day6:
     was present at the start of the day.
     
     In this example, after 18 days, there are a total of 26 fish. After 80 da ys, there would be a total of 5934.
+
+    --- Part Two ---
+    Suppose the lantern-fish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+    After 256 days in the example above, there would be a total of 26984457539 lantern-fish!
+
+    How many lantern-fish would there be after 256 days?
     """
+
     def __init__(self):
         with open("Data/Day6Data.txt", "r") as f:
             self.data = [*map(int, f.read().split(','))]
@@ -94,6 +106,102 @@ class Day6:
         print(sum(self.countAtAge))
 
 
+class Day7:
+    """
+    --- Day 7: The Treachery of Whales ---
+    A giant whale has decided your submarine is its next meal, and it's much
+    faster than you are. There's nowhere to run!
+
+    Suddenly, a swarm of crabs (each in its own tiny submarine - it's too deep for them otherwise) zooms in to rescue
+    you! They seem to be preparing to blast a hole in the ocean floor; sensors indicate a massive underground cave
+    system just beyond where they're aiming!
+
+    The crab submarines all need to be aligned before they'll have enough power to blast a large enough hole for your
+    submarine to get through. However, it doesn't look like they'll be aligned before the whale catches you! Maybe
+    you can help?
+
+    There's one major catch - crab submarines can only move horizontally.
+
+    You quickly make a list of the horizontal position of each crab (your puzzle input). Crab submarines have limited
+    fuel, so you need to find a way to make all of their horizontal positions match while requiring them to spend as
+    little fuel as possible.
+
+    For example, consider the following horizontal positions:
+
+    16,1,2,0,4,2,7,1,2,14
+
+    This means there's a crab with horizontal position 16, a crab with horizontal position 1, and so on.
+
+    Each change of 1 step in horizontal position of a single crab costs 1 fuel. You could choose any horizontal
+    position to align them all on, but the one that costs the least fuel is horizontal position 2:
+
+    Move from 16 to 2: 14 fuel
+    Move from 1 to 2: 1 fuel
+    Move from 2 to 2: 0 fuel
+    Move from 0 to 2: 2 fuel
+    Move from 4 to 2: 2 fuel
+    Move from 2 to 2: 0 fuel
+    Move from 7 to 2: 5 fuel
+    Move from 1 to 2: 1 fuel
+    Move from 2 to 2: 0 fuel
+    Move from 14 to 2: 12 fuel
+
+    This costs a total of 37 fuel. This is the cheapest possible outcome; more expensive outcomes include aligning at
+    position 1 (41 fuel), position 3 (39 fuel), or position 10 (71 fuel).
+
+    --- Part Two ---
+    The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+
+    As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in
+    horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2,
+    the third step costs 3, and so on.
+
+    As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align
+    them all on; in the example above, this becomes 5:
+
+    Move from 16 to 5: 66 fuel
+    Move from 1 to 5: 10 fuel
+    Move from 2 to 5: 6 fuel
+    Move from 0 to 5: 15 fuel
+    Move from 4 to 5: 1 fuel
+    Move from 2 to 5: 6 fuel
+    Move from 7 to 5: 3 fuel
+    Move from 1 to 5: 10 fuel
+    Move from 2 to 5: 6 fuel
+    Move from 14 to 5: 45 fuel
+
+    This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now
+    costs 206 fuel instead.
+
+    """
+
+    def __init__(self):
+        with open("Data/Day7Data.txt", "r") as f:
+            self.data = [*map(int, f.read().split(','))]
+            self.crabsAtLocation = [self.data.count(i) for i in range(max(self.data) + 1)]
+
+    def part1(self):
+        optimumLocation = np.median(self.data)
+        fuel = 0
+
+        for location, n in enumerate(self.crabsAtLocation):
+            fuel += n * abs(location - optimumLocation)
+
+        return fuel
+
+    def part2(self):
+        optimumLocation = math.floor(np.mean(self.data))
+        fuel = 0
+        sumToN = lambda num: (num * (num + 1)) / 2
+
+        for location, n in enumerate(self.crabsAtLocation):
+            fuel += n * sumToN(abs(location - optimumLocation))
+
+        return fuel
+
+        # print(self.crabsAtLocation, self.crabsAtLocation.index(max(self.crabsAtLocation)))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(Day6().part2())
+    print(Day7().part2())
